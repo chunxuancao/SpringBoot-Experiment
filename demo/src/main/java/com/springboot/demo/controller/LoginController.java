8c873e8b4de7ce2c9c468d6e98bf375870be7cbe
“chunxuancao” <cao18373503328@outlook.com>
2023-03-24 23:14:52 +0800

    4.0代码提交

---
+++ demo/src/main/java/com/springboot/demo/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.springboot.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Calendar;
+
+@Controller
+public class LoginController {
+    @GetMapping("/")
+    public String toLoginPage(Model model) {
+        model.addAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
+        return "login";
+    }
+}
