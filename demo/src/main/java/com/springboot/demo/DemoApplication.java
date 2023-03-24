c443dc9e94d872ad2a3c590bbbac6bf0afd825ee
“chunxuancao” <cao18373503328@outlook.com>
2023-03-25 01:35:49 +0800

    release-5.0的提交，其他版本在git tag中，或者通过git branch来切换分支操作

--- demo/src/main/java/com/springboot/demo/DemoApplication.java
+++ demo/src/main/java/com/springboot/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	// TODO: 运行前请进行重新加载maven项目
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
