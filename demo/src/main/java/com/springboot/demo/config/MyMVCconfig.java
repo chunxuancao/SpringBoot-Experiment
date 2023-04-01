984ea7b01d2992e74fd53c09e821da1b2e272188
“chunxuancao” <cao18373503328@outlook.com>
2023-04-01 16:04:22 +0800

    开始个人博客的开发

--- demo/src/main/java/com/springboot/demo/config/MyMVCconfig.java
+++ demo/src/main/java/com/springboot/demo/config/MyMVCconfig.java
@@ -17,6 +17,8 @@ public class MyMVCconfig implements WebMvcConfigurer {
         // 请求toLoginPage映射路径或者login.html页面都会自动映射到login.html页面
         registry.addViewController("/toLoginPage").setViewName("login");
         registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/").setViewName("index");
     }
 
     @Autowired
